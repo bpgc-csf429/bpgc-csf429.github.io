17eb1830b3aa12ad9d5c3b6564b344ba22a2d53a
someshsingh22 <someshsingh9414@gmail.com>
2021-07-26 02:02:35 +0530

    Directories Updated

--- assets/notebooks/add_button.py
+++ assets/notebooks/add_button.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     html = sys.argv[1]
     ipynb = sys.argv[2]
     flag = 0
-    button = """\n<p align="center"><a href="https://colab.research.google.com/github/bpgc-csf429/bpgc-csf429.github.io/blob/master/assets/notebooks/test2.ipynb"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Italian Trulli"></a></p>\n"""
+    button = """\n<p align="center"><a href="https://colab.research.google.com/github/bpgc-csf429/bpgc-csf429.github.io/blob/master/assets/notebooks/ipynb/test2.ipynb"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Italian Trulli"></a></p>\n"""
     button = button.replace("test2.ipynb", ipynb)
     with open(html, "r+") as f:
         lines = f.readlines()
--- assets/notebooks/readme.md
+++ assets/notebooks/readme.md
@@ -9,12 +9,17 @@ Requirements:
 bash converter.sh name_of_notebook1.ipynb name_of_notebook2.ipynb #and so on
 ```
 
-If you are doing it on collaboratory copy this snippet
+If you are doing it on collaboratory authenticate github 
+```bash
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+```
 
+and copy this snippet
 ```python
 !git clone https://github.com/bpgc-csf429/bpgc-csf429.github.io
 %cd /content/bpgc-csf429.github.io/assets/notebooks
 !bash converter.sh name_of_notebook1.ipynb name_of_notebook2.ipynb #and so on
 ```
 
-Push the html files!
+Add, commit, and push the html files!
