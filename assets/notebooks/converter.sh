f29c80d0bcbfa2beea1896d03c6c7116157a5c9a
Somesh Singh <someshsingh9414@gmail.com>
2021-07-25 20:54:29 +0000

    Fix HTML Notebooks Button

--- assets/notebooks/converter.sh
+++ assets/notebooks/converter.sh
@@ -1,4 +1,5 @@
 for notebook in "$@" 
 do
-    jupyter nbconvert --to html "ipynb/"$notebook --output-dir ./pages;
+    jupyter nbconvert --to html "ipynb/"$notebook".ipynb" --output-dir ./pages;
+    python3 add_button.py "pages/"$notebook".html" "ipynb/"$notebook".ipynb"
 done
